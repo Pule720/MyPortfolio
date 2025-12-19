a5fbe48d750dfb78e92a74ed8017ca9766f3f1ef
Pule Nxasana <pulenxasana720@gmail.com>
2025-12-19 18:44:04 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-os/", // This MUST match your repository name on GitHub
 })
